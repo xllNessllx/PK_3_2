10346a9730a6d9011dd2a0dedaf5745f7c87df6d
Sebastian M <sebi.markus@googlemail.com>
2019-11-07 15:42:47 +0100

    Initial commit

---
+++ PK_3_2/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * PK_3_2.cpp
+ *
+ * Created: 07.11.2019 15:41:00
+ * Author : Ness
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define F_CPU 1000000UL
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
