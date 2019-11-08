d8b9a286235d055ff85e92a804e145aaf712d05d
Sebastian M <sebi.markus@googlemail.com>
2019-11-08 13:41:44 +0100

    done?

--- PK_3_2/main.cpp
+++ PK_3_2/main.cpp
@@ -10,13 +10,58 @@
 #include <util/delay.h>
 
 #define F_CPU 1000000UL
+bool red = false;
+bool yellow = false;
 
+//PIND 2
+ISR(INT0_vect){
+	red = true;
+	yellow false;
+}
+
+//PIND 3
+ISR(INT1_vect){
+	red = false;
+	yellow = true;
+}
+
+void init(){
+	DDRB = 0b11111111;
+	DDRC = 0b00000000;
+	
+	PORTC = 0xFF;
+}
+
+void ledRed(){
+	PORTB = (1 << PB0);
+	_delay_ms(200);
+	PORTB = (0 << PB0);
+	_delay_ms(200);
+}
+
+void ledYellow(){
+	PORTB = (1 << PB1);
+	_delay_ms(200);
+	PORTB = (0 << PB1);
+	_delay_ms(200);
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    
+	init()
+	
     while (1) 
     {
+		if(red && !yellow){
+			ledRed();
+		}
+		else if(!red && yellow){
+			ledYellow();
+		}
+		else if(!red && !yellow){
+			//Kein Switch aktiviert
+		}
     }
 }
 
